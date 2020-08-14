5c68e174a1982e0bf9600d7b58e9ffb7e014be38
Paul Skeppar <paulbertilskeppar@gmail.com>
2020-08-14 17:36:49 +0200

    added options to run in server env

--- utils/crawler.js
+++ utils/crawler.js
@@ -1,17 +1,20 @@
 const puppeteer = require('puppeteer');
 
+let puppeteerConfig = process.env.NODE_ENV === 'production' ?
+    { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] } : { headless: true }
+
 
 const takeScreenshot = async (url, device) => {
-
+    // choose headless browser width & hight
     let options = device === 'phone' ?
         { width: 375, height: 812, deviceScaleFactor: 2 } : device === "laptop" ?
             { width: 1440, height: 900, deviceScaleFactor: 2 } : { width: 1920, height: 1080, deviceScaleFactor: 2 }
 
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage();
-    // deviceScaleFactor makes the image quality much better
-    await page.setViewport(options);
     try {
+        const browser = await puppeteer.launch(puppeteerConfig)
+        const page = await browser.newPage();
+        // deviceScaleFactor makes the image quality much better
+        await page.setViewport(options);
         await page.goto(`https://${url}`);
         // wait for pageload
         await page.waitFor(2000);
